8c3687d662b8f3130ceed8ea97cabfe785e45135
Ista Zahn <istazahn@gmail.com>
2021-09-14 17:36:02 -0400

    Cleanup windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,9 +6,3 @@ set "MAKE_ARGS=ARCH=%ARCH% CC=%CC% OS=%OS% SHLIB_EXT=dll prefix=%LIBRARY_PREFIX:
 
 %MAKE% %MAKE_ARGS%
 %MAKE% install %MAKE_ARGS%
-
-REM Its makefile still don't know how to generate .lib instead of .a:
-REM This is the "static library"
-move %LIBRARY_LIB%\libopenlibm.a %LIBRARY_LIB%\libopenlibm_static.lib
-REM This is the "import library", note that it is moved to LIBRARY_LIB
-move %LIBRARY_BIN%\libopenlibm.dll.a %LIBRARY_LIB%\libopenlibm.lib
